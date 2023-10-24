8b6dd39e180e0a4404e03ae28ca1267f55c2d3dd
aliudovik <146223146+aliudovik@users.noreply.github.com>
2023-10-24 12:27:42 +0000

    changes

--- functions1.c
+++ functions1.c
@@ -10,7 +10,7 @@ int sum (int a, int b)
 int main () 
 {
     int y = 2;
-    int z = sum(5, y); // calling our new function
+    int z = sum(sum(354, 7), 7); // calling our new function
 
     printf ("The sum of 5 and %d is %d\n", y, z);
 }
--- read_from_file.c
+++ read_from_file.c
@@ -11,7 +11,7 @@ int main() {
     int buffer_size = 100;
     char line_buffer[buffer_size];
     while (fgets(line_buffer, buffer_size, file) != NULL) {
-        printf("%d\n", atoi(line_buffer));
+        printf("%s", line_buffer);
     }
 
     fclose(file);
