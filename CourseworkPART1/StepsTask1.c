f155e8f533ee8b90bc23ee600bea522d73e536f8
aliudovik <146223146+aliudovik@users.noreply.github.com>
2023-10-29 22:19:23 +0000

    changes

--- CourseworkPART1/StepsTask1.c
+++ CourseworkPART1/StepsTask1.c
@@ -43,6 +43,48 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
+    char filename [] = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("");
+        return 1;
+    }
+    
+    int a = 0;
+    int record_size = 100;
+    char record[record_size];
+    while (fgets(record, record_size, file) != NULL) {
+        a++;
+    //    tokeniseRecord(line_buffer, ",", date, time, steps);
+    //    FITNESS_DATA one;
+    //    strcpy(one.date, date);
+    //    strcpy(one.time, time);
+    //    strcpy(one.steps, steps);
+    //    printf("%s", one.date);
+    }
 
+    //char record[21] = "2023-09-01,07:30,300";
+    char date[11];
+    char time[6];
+    char steps[10];  // Large enough to hold a typical step count as a string
+
+    int stepsint;
+    
+    tokeniseRecord(record, ",", date, time, steps);
+    
+    printf("Date: %s\n", date);
+    printf("Time: %s\n", time);
+    printf("Steps: %s\n", steps);
+
+    struct FITNESS_DATA
+
+    // Convert the steps string to an integer
+    stepsint = atoi(steps);
+    printf("Steps as an integer: %d\n", stepsint);
+    
+    printf("Number of records in file: %d\n", a);
+
+    fclose(file);
+    return 0;
 
 }
