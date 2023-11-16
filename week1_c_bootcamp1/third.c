a618e1bf24c86e3abadedb1a644ebdbd85c0f013
aliudovik <146223146+aliudovik@users.noreply.github.com>
2023-11-16 15:15:48 +0000

    changes

--- week1_c_bootcamp1/third.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-    
-    int main() {
-    int age;
-    printf("Hello, please enter your age: ");
-    scanf("%d", &age);
-    printf("Your age is %d years\n", age);
-    return 0;
-}
